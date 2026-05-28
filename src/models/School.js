97ea5bc1ce385529cf1640cd148cae0a16d59a37
CharlesXavier1126 <newzealand1126@gmail.com>
2026-05-28 13:47:57 +1200

    add schools table

---
+++ src/models/School.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index').sequelize;
+
+const School = sequelize.define('School', {
+  id: {
+    type: DataTypes.CHAR(6),
+    primaryKey: true,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  name: { type: DataTypes.STRING },
+  address: { type: DataTypes.STRING },
+  phone: { type: DataTypes.STRING },
+  email: { type: DataTypes.STRING },
+  latitude: { type: DataTypes.DECIMAL(10, 6) },
+  longitude: { type: DataTypes.DECIMAL(10, 6) },
+  image1: { type: DataTypes.STRING },
+  quota: { type: DataTypes.INTEGER },
+  comment: { type: DataTypes.TEXT }
+}, {
+  tableName: 'schools'
+});
+
+module.exports = School;
